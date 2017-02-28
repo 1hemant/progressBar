3c535d0465be04a58806dd832901b919fbc05fd9
1hemant <demonvsgod@gmail.com>
2017-02-28 13:51:50 +0530

    Final
    Done

--- www/js/index.js
+++ www/js/index.js
@@ -1,56 +1,51 @@
 var width = 40;
 var elem;
 $("select").change(function(e) {
-  elem = $('#' + $(this).val().trim())
+    elem = $('#' + $(this).val().trim());
 }).trigger("change");
 
-$('button').on('click', function(e) {
-  e.preventDefault();
-  var data = $(this).data();
-  var width = Number((elem.width() / elem.parent().width() * 100).toFixed(0));
-  //debugger;
-  switch (data.operator) {
-    case '-':
-      if ((width) > 0) {
-        var calculated = (width - data.width) + "%"
-        elem.width(calculated).text(calculated);
-          if((width - data.width) < 60 && (width - data.width) > 50 ){
-              elem[0].className="progress-bar progress-bar-info";
-          }
-          else if((width - data.width) > 60  && (width - data.width) < 70 ){
-              elem[0].className="progress-bar progress-bar-warning";
-          }
-          else if((width - data.width) > 70){
-              elem[0].className="progress-bar progress-bar-danger";
-          }
-          else if((width - data.width) < 50){
-              elem[0].className="progress-bar progress-bar-success";
-          }
-          else if((width - data.width) < 0){
-            var calculated = (0) + "%"
-            elem.width(calculated).text(0);
-          }
-      } 
-      break;
-    case '+':
-      if ((width) < 100) {
-        var calculated = (width + data.width) + "%"
-        elem.width(calculated).text(calculated);
-          if((width + data.width) > 50 && (width + data.width) < 60 ){
-              elem[0].className="progress-bar progress-bar-info";
-          }
-          else if((width + data.width) > 60  && (width + data.width) < 70 ){
-              elem[0].className="progress-bar progress-bar-warning";
-          }
-          else if((width + data.width) > 70){
-              elem[0].className="progress-bar progress-bar-danger";
-          }
-          else if((width + data.width) > 100){
-            var calculated = (100) + "%"
-            elem.width(calculated).text(100);
-          }
-      }
-      break;
-  }
+$('button').on('click', function(e) { //find the button s per the click target
+    e.preventDefault();
+    var data = $(this).data();
+    var width = Number((elem.width() / elem.parent().width() * 100).toFixed(0)); //Find the width
+    var calculated;
+    switch (data.operator) { //get the data operator and run the switch
+        case '-':
+            if ((width) > 0) {
+                calculated = (width - data.width) + "%";
+                elem.width(calculated).text(calculated);
+                if ((width - data.width) < 60 && (width - data.width) > 50) {
+                    elem[0].className = "progress-bar progress-bar-info";
+                } else if ((width - data.width) > 60 && (width - data.width) < 70) {
+                    elem[0].className = "progress-bar progress-bar-warning";
+                } else if ((width - data.width) > 70) {
+                    elem[0].className = "progress-bar progress-bar-danger";
+                } else if ((width - data.width) < 50) {
+                    elem[0].className = "progress-bar progress-bar-success";
+                } 
+            }
+             if ((width - data.width) < 0) {
+                    calculated = (0) + "%";
+                    elem.width(calculated).text(0);
+                }
+            break;
+        case '+':
+            if ((width) < 100) {
+                calculated = (width + data.width) + "%";
+                elem.width(calculated).text(calculated);
+                if ((width + data.width) > 50 && (width + data.width) < 60) {
+                    elem[0].className = "progress-bar progress-bar-info";
+                } else if ((width + data.width) > 60 && (width + data.width) < 70) {
+                    elem[0].className = "progress-bar progress-bar-warning";
+                } else if ((width + data.width) > 70) {
+                    elem[0].className = "progress-bar progress-bar-danger";
+                } 
+            }
+            if ((width + data.width) > 100) {
+                    calculated = (100) + "%";
+                    elem.width(calculated).text(100);
+                }
+            break;
+    }
 
 });
